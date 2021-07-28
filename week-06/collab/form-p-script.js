12f7e77dcd7c2e341d85c7e189d0e46daca3671d
bubbles linux <appenzeller.barbara@gmail.com>
2021-07-28 01:26:56 +0100

    Form Practice: text length

--- week-06/collab/form-p-script.js
+++ week-06/collab/form-p-script.js
@@ -12,15 +12,16 @@ const teamAddpasswordField = document.querySelector('#team-add-admin-pass');
 const teamAddSubmit = document.querySelector('#team-add-submit');
 const teamAddName = document.querySelector('#team-add-name');
 
-function passwordChecker() {
+function passwordChecker(field, correct) {
   // console.log(teamAddName); //value: "Heidi"
   console.log(teamAddName.value); //"Heidi"
   console.log(teamAddpasswordField.value);
-  const check = teamAddpasswordField.value === adminPasswordBAgency;
+  // const check = teamAddpasswordField.value === adminPasswordBAgency;
+  const check = field.value === correct;
   console.log(check);
   if (!check) {
     console.log('incorrect password');
-    //How reject form submission?
+    //How reject form submission? --> formnovalidate?
     //Display "Incorrect Admin Password" in HTML
   } else {
     console.log('correct password');
@@ -29,13 +30,36 @@ function passwordChecker() {
 }
 
 teamAddSubmit.addEventListener('click', () => {
-  passwordChecker();
+  passwordChecker(teamAddpasswordField, adminPasswordBAgency);
 });
 
 /*149 CHARACTERS
 Be notified if the description I’m writing is **too long** (E2)
-  - check length
+  - access textarea --> text input
+  - check length, compare to max
+  - EventListener realtime
+  - feedback: red border
 */
+const teamAddDesc = document.querySelector('#team-add-desc');
+
+function textLengthChecker(field, max) {
+  /*Option 2
+  const length = field.value.length;
+  field.setAttribute('aria-invalid', length > 149 ? true : false);
+  */
+  console.log(`Max is ${max}`);
+  console.log(field.textLength);
+  if (field.textLength > max) {
+    field.setAttribute('aria-invalid', 'true');
+    //What happens with aria-invalid? --> Display "too long" --> .append, .create
+  } else {
+    field.setAttribute('aria-invalid', 'false');
+  }
+}
+
+teamAddDesc.addEventListener('input', () => {
+  textLengthChecker(teamAddDesc, 149);
+});
 
 /* TEMPLATE
 **See** the new member on the page once I click a button (E3)
