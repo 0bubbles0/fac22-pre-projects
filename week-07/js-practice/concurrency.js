25677bee3040441861b914cb7595aca1177c7201
bubbles linux <appenzeller.barbara@gmail.com>
2021-08-09 12:56:27 +0100

    Concurrency Notes

--- week-07/js-practice/concurrency.js
+++ week-07/js-practice/concurrency.js
@@ -2,6 +2,22 @@ const promise = Promise.resolve({ name: 'Amir' }); // Promise { {name: 'Amir'}}
 
 Object.assign({}, promise); // {}
 JSON.parse(JSON.stringify(Promise.resolve().then(() => 5))); // {}
-Promise.reject(new Error('it failed')); // {rejected: 'Error: not found'}
+// Promise.reject(new Error('it failed')); // {rejected: 'Error: not found'}
 
-console.log();
+const users = [
+  { id: 1, name: 'Amir' },
+  { id: 2, name: 'Betty' },
+];
+
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    const user = users.find((user) => user.id === id);
+    if (user) {
+      resolve(user);
+    } else {
+      reject(new Error('no such user'));
+    }
+  });
+}
+
+getUser(1);
