9e8eccb86a448be75acd462b9a4fbe167e07c38d
bubbles linux <appenzeller.barbara@gmail.com>
2021-08-18 16:51:01 +0100

    Execute ModernJS: practice number meth

--- week-09/js/number-meth.js
+++ week-09/js/number-meth.js
@@ -1,12 +1,12 @@
-function add(x, y) {
-  return x + y;
-}
+// function add(x, y) {
+//   return x + y;
+// }
 
-const numbers = [1, 2];
-console.log(add(...numbers));
+// const numbers = [1, 2];
+// console.log(add(...numbers));
 
-function add(...numbers) {
-  return;
-}
-const numbers2 = [1, 2, 3];
-console.log(add(...numbers2));
+// function add(...numbers) {
+//   return;
+// }
+// const numbers2 = [1, 2, 3];
+// console.log(add(...numbers2));
